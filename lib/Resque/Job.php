bfd1e1d7dd2b2bd453b985e32e28c3e2d4bcb991
Vũ Đình Khôi <o0khoiclub0o@gmail.com>
2015-05-27 10:59:19 +0700

    Fix lib

--- lib/Resque/Job.php
+++ lib/Resque/Job.php
@@ -52,11 +52,11 @@ class Resque_Job
 	 */
 	public static function create($queue, $class, $args = null, $monitor = false)
 	{
-		if($args !== null && !is_array($args)) {
-			throw new InvalidArgumentException(
-				'Supplied $args must be an array.'
-			);
-		}
+		// if($args !== null && !is_array($args)) {
+		// 	throw new InvalidArgumentException(
+		// 		'Supplied $args must be an array.'
+		// 	);
+		// }
 		$id = md5(uniqid('', true));
 		Resque::push($queue, array(
 			'class'	=> $class,
